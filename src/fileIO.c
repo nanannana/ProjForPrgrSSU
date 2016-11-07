820bb23c6709ec14a7fff973b6315f05051b426b
nanannana <eogh1209@naver.com>
2016-11-07 23:29:04 +0900

    ㅠㅠ

--- src/fileIO.c
+++ src/fileIO.c
@@ -4,50 +4,228 @@
 #include"fileIO.h"
 #include<string.h>
 
+char temp[200];
+
+void client_f_s(FILE *client_fp, Client *client)
+{
+	char trash = '\n';
+	int i = 0;
+	trash = fgetc(client_fp);
+	while(trash == '|')
+	{
+		trash = fgetc(client_fp);
+	}
+	while(trash != '|')
+	{
+		temp[i++] = trash;
+		trash = fgetc(client_fp);
+	}
+}
+void book_f_s(FILE *book_fp ,Book * book)
+{
+	char trash = '\n';
+	int i = 0;
+	trash = fgetc(book_fp);
+	while(trash == '|')
+	{
+		trash = fgetc(book_fp);
+	}
+	while(trash != '|') // current error!!
+	{
+		temp[i++] = trash;
+		trash = fgetc(book_fp);
+	}
+	printf("%s\n",temp);
+}
+void borrow_f_s(FILE *borrow_fp, Borrow *borrow)
+{
+	printf("borrow\n");
+}
+
+int what_struct(void *fp ,void * vsp, int n)
+{
+	int i =0;
+	char a;
+	if(n==1)
+	{
+		Client *sp = vsp;
+		for(i=0; i<5; i++)
+		{
+			memset(temp,0,sizeof(temp));
+			client_f_s(fp,sp);
+			switch(i)
+			{
+				case 0:
+					sp -> sch_num = atoi(temp);
+					break;
+				case 1:
+					sp -> name = malloc( strlen(temp) + 1 );
+					strcpy(sp -> name, temp);
+					break;
+				case 2:
+					sp -> password = malloc( strlen(temp) + 1 );
+					strcpy(sp -> password, temp);
+					break;
+				case 3:
+					sp -> address = malloc( strlen(temp) + 1 );
+					strcpy(sp -> address, temp);
+					break;
+				case 4:
+					sp -> phone_num = malloc( strlen(temp) + 1 );
+					strcpy(sp -> phone_num, temp);
+					a = fgetc(fp);
+					fseek(fp,-1, SEEK_CUR);
+					break;
+			}
+		}
+		if(a == EOF)
+		{
+			return a;
+		}
+		else
+			fseek(fp , 1 , SEEK_CUR);
+
+	}
+	else if(n==2)
+	{
+		borrow_f_s(fp,vsp);
+
+		return 1;
+	}
+	else if(n==3)
+	{
+		Book *sp = vsp;
+		for(i=0; i<7; i++)
+		{
+			memset(temp,0,sizeof(temp));
+			book_f_s(fp,sp);
+			
+			switch(i)
+			{
+				case 0:
+				/*	if(strlen(temp) != 7)
+						printf("Data eorror!!\n");
+					else*/
+						sp -> book_num = atoi(temp);
+					break;
+				case 1:
+				/*	if(strlen(temp) != 13)
+						printf("Data eorror!!\n");
+					else*/
+						sp -> ISBN = atoi(temp);
+					break;
+				case 2:
+					sp -> name = malloc( strlen(temp) + 1 );
+					strcpy(sp -> name, temp);
+					break;
+				case 3:
+					sp -> publisher = malloc( strlen(temp) + 1 );
+					strcpy(sp -> publisher, temp);
+					break;
+				case 4:
+					sp -> author = malloc( strlen(temp) + 1 );
+					strcpy(sp -> author, temp);
+					break;
+				case 5:
+					sp -> owner = malloc( strlen(temp) + 1 );
+					strcpy(sp -> owner, temp);
+				case 6:
+					sp -> borrow_Y_N = malloc( strlen(temp) + 1 );
+					strcpy(sp -> borrow_Y_N, temp);
+					a = fgetc(fp);
+					fseek(fp,-1, SEEK_CUR);
+					break;
+			}
+	
+		}
+		if(a == EOF)
+		{
+			return a;
+		}
+		else
+		{
+			fseek(fp , 1 , SEEK_CUR);
+		}
+	}
+	else
+	{
+		printf("ERORR!!!\n");
+		return 0;
+	}
+	return 0;
+}
+
+void file_data_struct(Client *client, Book *book, Borrow *borrow)
+{
+	FILE *client_fp, *borrow_fp, *book_fp; // 순서대로 client. borrow, book
+	Client *client_current;
+	Book *book_current;
+	Borrow *borrow_current;
+	int check = 1;	
+	
+	client_fp = fopen("client.txt", "r");
+	borrow_fp = fopen("borrow.txt", "r");
+	book_fp = fopen("book.txt", "r");
+	
+	client_current = client;
+	while(check != -1)
+	{
+		client_current -> next = (Client *)malloc(sizeof(Client));
+		client_current = client_current -> next;
+		check =what_struct((void *)client_fp ,(void *)client_current, 1) ;
+	}
+	check = 1;
+
+	borrow_current = borrow;
+	while(ftell(borrow_fp) > EOF)
+	{
+		
+		break;	
+	}
+	check = 1;
+
+	book_current = book;
+	while(check != -1)
+	{
+		book_current -> next = (Book *)malloc(sizeof(Book));
+		book_current -> next -> last = book_current;
+		book_current = book_current -> next;
+		check = what_struct((void *)book_fp ,(void *)book_current, 3);
+	}
+	check = 1;
+
+
+
+	fclose(client_fp);
+	fclose(borrow_fp);
+	fclose(book_fp);
+	
+}
+
+void client_struct_data_monitor(Client *client)
+{
+	Client *current;
+	current = client -> next;
+	int i = 1;
+	while(1)
+	{
+		printf("%d번쨰 데이터\n",i++);
+		printf("|%d|%s|%s",current -> sch_num, current -> name, current -> password);
+		printf("|%s|%s|\n",current -> address, current -> phone_num);
+	
+		if(current -> next)
+			current= current -> next;
+		else
+			break;
+	}
+}
+
 int main(void)
 {
 	Client client;
 	Book book;
 	Borrow borrow;
-	FILE *cfp = fopen("client.txt","r");
-	input_data(cfp, &client, &book, &borrow);
-	fclose(cfp);
+	file_data_struct(&client, &book, &borrow);
+	client_struct_data_monitor(&client);
 	return 0;
-}
-
-void input_data(FILE * cfp,Client *client, Book *book, Borrow *borrow)
-{
-	int i = 0, end, k;
-	char sp = 46;
-	char temp[300] = {'\0'};
-	
-	fseek(cfp , 0 , SEEK_END);
-	end = ftell(cfp);
-	rewind(cfp);
-	
-	while(ftell(cfp) < end)
-	{
-		client -> next = (Client *)malloc(sizeof(Client));
-		client = client -> next;
-		client -> next = NULL;
-		sp =fgetc(cfp);
-		while(sp != '\n')
-		{
-			while(sp == '|' || sp == ' ' )
-			{
-				sp = fgetc(cfp);
-			}
-			fseek(cfp, -1, SEEK_CUR);
-			fscanf(cfp,"%d",&(client -> sch_num));
-			break;
-		}
-		break;
-	}
-	printf("%d\n", client -> sch_num);
-}
-
-void print_data(Client *client, Book *book, Borrow *borrow)
-{
-	
-	
 }
--- src/fileIO.h
+++ src/fileIO.h
@@ -4,32 +4,33 @@
 
 typedef struct client
 {
-	int sch_num;
-    char *name;
-   	char *password;
-    char *address;
-    char *phone_num;
-    struct client *next;
+	int sch_num; // 학번
+    char *name; // 이름 
+   	char *password; // 비밀번호 
+    char *address; // 주소
+    char *phone_num; // 연락처 
+    struct client *next; // 연결리스트 다음 노드를 가리킬 것임
 }Client;
 
 typedef struct book
 {
-	int book_num; // 7
-	int ISBN;	// 13
-	char *name;
-	char *publisher;
-	char *author;
-	char *owner;
-	char borrow_Y_N;
-	struct Book *next;
+	int book_num; // 7자리 책 번호
+	int ISBN;	// 13자리 ISBN 번호 
+	char *name; // 책 이름 
+	char *publisher; // 출판사 
+	char *author; // 저자
+	char *owner; // 소장처
+	char *borrow_Y_N; // 대여 가능여부
+	struct book *next; // 다음 노드
+	struct book *last; // 이전 노드
 }Book;
 
 typedef struct borrow
 {
-	int sch_num;
-	int book_num;
-	time_t borrow_day;
-	time_t return_day;
-	struct Borrow *next;
+	int sch_num; // 빌린사람 학번
+	int book_num; // 빌려간 책
+	time_t borrow_day; // 빌린날
+	time_t return_day; // 반날해야 하는 날
+	struct Borrow *next; // 다음 노드
 }Borrow;
 
