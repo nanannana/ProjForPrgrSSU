8bad2a04b1636ae58e1a96eb7f692f5d1a881edd
Pragion345 <jms100300@naver.com>
2016-11-13 14:38:57 +0900

    manageData.c DEBUG 코드 추가

--- src/manageData.c
+++ src/manageData.c
@@ -1,6 +1,17 @@
 #include<stdlib.h>
 #include"manageData.h"
 
+#ifdef DEBUG
+List_Client *list_client = {NULL};
+List_Book *list_book = {NULL};
+List_Borrow *list_borrow = {NULL};
+
+void file_write_client(void){return;}
+void file_write_book(void){return;}
+void file_write_borrow(void){return;}
+
+#endif 
+
 #define CODE(Sth, sth, key) 									\
 /* 이 c 파일에서만 사용되는 get함수입니다. 성공을 확인한 후 리스트의 current*/\
 /* 가져다 이용하시면 됩니다.												*/\
@@ -25,13 +36,20 @@ int append_##sth(Sth sth)										\
 	/* 리스트에 추가할 원소를 동적할당하여 공간을 만듭니다.*/	\
 	Sth * element = (Sth *)malloc(sizeof(Sth));					\
 	*element = sth; 											\
+																\
+	if (list_##sth->head == NULL)								\
+	{															\
+		list_##sth->head = element;								\
+		list_##sth->tail = element;								\
+		return Success;											\
+	}															\
     															\
 	if (private_get_##sth(element->key) == Success)				\
 	{/* 중복이라면 오류 반환 */									\
 		return Fail_Two_Same_Value;								\
 	}/* 현재 리스트의 current 는 삽입하기 적당한 위치 */		\
 																\
-	if (list_##sth->current->next == NULL)						\
+	if (list_##sth->current->next == NULL && list_##sth->current->key < element->key)\
 	{/* 마지막 원소일 때 */										\
 		list_##sth->tail = element;								\
 																\
@@ -74,15 +92,11 @@ int remove_##sth(int key)										\
 	{/* 마지막 원소 */											\
 		list_##sth->tail = list_##sth->current->last;			\
 		list_##sth->tail->next = NULL;							\
-																\
-		free(list_##sth->current);								\
 	}															\
 	else if (list_##sth->current->last == NULL)					\
 	{/* 첫번째 원소 */											\
 		list_##sth->head = list_##sth->current->next;			\
 		list_##sth->head->last = NULL;							\
-																\
-		free(list_##sth->current);								\
 	}															\
 	else														\
 	{/* 중간 원소 */											\
@@ -90,9 +104,8 @@ int remove_##sth(int key)										\
 																\
 		last->next = next;										\
 		next->last = last;										\
-																\
-		free(list_##sth->current);								\
 	}															\
+	free(list_##sth->current);									\
 																\
 	file_write_##sth();											\
 																\
@@ -124,15 +137,31 @@ int replace_##sth(const Sth* p_origin, Sth sth)					\
 		return Fail_No_Element;									\
 																\
 	/* 원소를 바꿔치기 합니다. */								\
-	Sth *last = p_origin->last, *next = p_origin->next;			\
+	if (p_origin->next == NULL)									\
+	{															\
+		Sth *last = p_origin->last;								\
 																\
-	last->next = element;										\
-	next->last = element;										\
+		last->next = element;									\
+		element->last = last;									\
+	}															\
+	else if(p_origin->last == NULL)								\
+	{															\
+		Sth *next = p_origin->next;								\
 																\
-	element->last = last;										\
-	element->next = next;										\
+		next->last = element;									\
+		element->next = next;									\
+	}															\
+	else														\
+	{															\
+		Sth *last = p_origin->last, *next = p_origin->next;		\
 																\
-	free((void *)p_origin);												\
+		last->next = element;									\
+		next->last = element;									\
+																\
+		element->last = last;									\
+		element->next = next;									\
+	}															\
+	free((void *)p_origin);										\
 																\
 	file_write_##sth();											\
 																\
@@ -146,9 +175,69 @@ CODE(Borrow, borrow, book_num)
 #ifdef DEBUG
 int main(void)
 {
+	int i = 0;
+	int j = 0;
+	int key = 0;
+	const Client* t = NULL;
 	while(1)
 	{
 		printf("디버깅 모드입니다.\n");
+		printf("1. 리스트 추가\n2. 리스트 제거\n3. 리스트 얻기\n4. 리스트 대체\n 5. 현재 리스트 출력\n");
+		scanf("%d",&i);
+
+		switch(i)
+		{
+			case 1:
+				printf("추가할 key 입력 : ");
+				scanf("%d", &key);
+				Client c = {key,0};
+				if (append_client(c) == Success)
+					printf("%d 추가 완료\n",key);
+				else
+					printf("추가 실패\n");
+				break;
+			case 2:
+				printf("제거할 key 입력 : ");
+				scanf("%d", &key);
+				if (remove_client(key) == Success)
+					printf("%d 제거 완료\n", key);
+				else
+					printf("제거 실패\n");
+				break;
+			case 3:
+				printf("얻을 key 입력 : ");
+				scanf("%d", &key);
+				if (get_client(key,&t) == Success)
+				{
+					printf("%d\n",t->sch_num);
+				}
+				else
+					printf("얻기 실패\n");
+				break;
+			case 4:
+				printf("원래 key 바꿀 key :");
+				scanf("%d %d",&key,&j);
+				Client f = {j,0};
+				if (get_client(key, &t) == Success)
+				{
+					if(replace_client(t,f) == Success)
+						printf("바꾸기 성공\n");
+					else
+						printf("바꾸기 실패\n");
+				}
+				else
+					printf("얻기 실패\n");
+				break;
+			case 5:
+				list_client->current = list_client->head;
+				printf("리스트 출력\n");
+				while(list_client->current != list_client->tail)
+				{
+					printf("%d\n",list_client->current->sch_num);
+					list_client->current = list_client->current->next;
+				}
+				break;
+		}
 	}
 
 	return 0;
