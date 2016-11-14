cf42ec35fcd07b4e9575ce5338180e9de925b8d2
Grapefruitsaft <und37d0g@gmail.com>
2016-11-14 23:17:26 +0900

    틀만 잡았음

--- src/menu.h
+++ src/menu.h
@@ -1,5 +1,34 @@
+#include <stdio.h>
 
 /// 프로그램의 본격적인 시작점입니다.
 /// 이 함수를 시작으로 메뉴 출력을 포함한 기능이 실행되게 됩니다.
 /// 0을 반환하며 정상적으로 종료합니다.
 int start_proc(void);
+
+//함수원형
+void Library_service();
+
+void Member_menu();
+
+void Search_books();
+void By_title();
+void By_publisher();
+void By_ISBN();
+void By_author();
+void Total_Search();
+
+void My_BB_list();
+void Modi_my_info();
+void Withdraw();
+
+void Admin_nemu();
+void Register_book();
+void Delete_book();
+void Lend_book();
+void Return_book();
+
+void Member_list();
+void Search_name();
+void Search_ID();
+void M_total_search();
+
