6e0e62028ba8178904e2177c6044628d12fd7da4
Pragion345 <jms100300@naver.com>
2016-11-30 12:34:41 +0900

    main 함수 버그 수정

--- src/main.c
+++ src/main.c
@@ -1,18 +1,26 @@
-#include<stdio.h>
 #include"fileIO.h"
 #include"login.h"
 #include"manageData.h"
 #include"menu.h"
 
+
+/// 프로그램 시작점
 int main(void)
 {
-	//파일을 읽어들여 전역변수 list를 할당합니다.
-    file_read();
+	// 리스트를 사용할 수 있게 초기화합니다.
+	init_all_list();
+
+	// 파일을 읽어들여 전역변수 list를 할당합니다.
+    get_all_file_data();
 
 	// 본격적인 프로시저를 시작합니다.
     start_proc();
 
-    // 리스트의 모든 값을 메모리에서 해제시킵니다.
+	// 끝날 때에는 리스트의 값을 파일로 저장시킵니다.
+	file_write();
+    
+	// 리스트의 모든 값을 메모리에서 해제시킵니다.
     free_all_lists();
+
     return 0;
 }
