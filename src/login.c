41407d82267d1b0069e89370a48fd6283251193d
Pragion345 <jms100300@naver.com>
2016-12-06 09:58:06 +0900

    데이터 정렬 및 사인 업 버그 수정

--- src/login.c
+++ src/login.c
@@ -30,13 +30,12 @@ void Sign_up(void)
 	Client client,client_temp;
 	const Client *compare = &client_temp;
 	char temp[500];
-	int sch_num;
 
-	printf(">>회원 가입<<\n");
+	printf("\n>>회원 가입<<\n");
 	printf("학번, 비밀번호, 이름, 주소, 전화번호를 입력하세요\n");
 	printf("학번: ");
 	scanf("%s",temp);
-	if (get_client(sch_num = atoi(temp),&compare) == Success)
+	if (get_client(atoi(temp),&compare) == Success)
 	{
 		printf("중복된 학번입니다");
 		return;
@@ -45,19 +44,24 @@ void Sign_up(void)
 	scanf("%s",temp);
 	client.name = (char *)malloc(sizeof(char) * (strlen(temp) + 1));
 	strcpy(client.name,temp);
-	
-	printf("전화번호: ");
+
+	printf("비밀번호: ");
 	scanf("%s",temp);
 	client.password = (char *)malloc(sizeof(char) * (strlen(temp) + 1));
 	strcpy(client.password,temp);
-
+	
 	printf("주소: ");
 	scanf("%s",temp);
 	client.address = (char *)malloc(sizeof(char) * (strlen(temp) + 1));
 	strcpy(client.address,temp);
 
+	printf("전화번호: ");
+	scanf("%s",temp);
+	client.password = (char *)malloc(sizeof(char) * (strlen(temp) + 1));
+	strcpy(client.password,temp);
+
 	append_client(client);
-	printf("회원가입 되셨습니다.");
+	printf("회원가입 되셨습니다.\n");
 
 
 	return ;
